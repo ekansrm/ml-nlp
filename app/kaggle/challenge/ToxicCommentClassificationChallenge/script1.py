e79fdb042edfa9b01de9461f4f192ad142d51158
ekansrm <ekansrm0001@163.com>
2018-02-09 17:39:57 +0800

    代码整理

--- app/kaggle/challenge/ToxicCommentClassificationChallenge/script1.py
+++ app/kaggle/challenge/ToxicCommentClassificationChallenge/script1.py
@@ -1,33 +1,28 @@
-from data.kaggle.challenge.ToxicCommentClassification import train_filepath, test_filepath, filepath
 
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
-
-# Any results you write to the current directory are saved as output.
-
-from keras.models import Model
-from keras.layers import Dense, Embedding, Input
+import os
+import json
+from keras.models import Model, load_model
+from keras.layers import Embedding, Input
 from keras.layers import LSTM, Bidirectional, GlobalMaxPool1D, Dropout
 from keras.preprocessing import text, sequence
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 
+from data.kaggle.challenge.ToxicCommentClassification import train_filepath, test_filepath, filepath
+
 max_features = 20000
 maxlen = 100
 
-
 train = pd.read_csv(train_filepath)
 test = pd.read_csv(test_filepath)
-train = train.sample(frac=1)
+train = train.sample(frac=0.001)
+test = test.sample(frac=0.001)
 
 list_sentences_train = train["comment_text"].fillna("CVxTz").values
 list_classes = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
 y = train[list_classes].values
 list_sentences_test = test["comment_text"].fillna("CVxTz").values
-
 
 tokenizer = text.Tokenizer(num_words=max_features)
 tokenizer.fit_on_texts(list(list_sentences_train))
@@ -138,23 +133,42 @@ class EmbeddingBiLSTM(object):
         return dense
 
 
-class RunnerBiLSTM(object):
+class Runner(object):
+    """
+    Runner的职责是负责整个环境的管理和持久化
 
-    def __init__(self, model, save_path):
+    """
+
+    model_filename = 'model'
+    params_filename = 'params'
+
+    def __init__(self, model, save_dir, inputs_len, inputs_vocab, outputs_class):
         self._model = model
-        self._save_path = save_path
+        self._save_dir = save_dir
+        self._inputs_len = inputs_len
+        self._inputs_vocab = inputs_vocab
+        self._outputs_class = outputs_class
+        self._idx = None
+        self._x = None
+        self._y = None
 
     @classmethod
-    def build(cls, save_path):
+    def init(cls, save_dir, inputs_len, inputs_vocab, outputs_class):
         """
-        :param self:
+        :param params_filename:
+        :param model_filename:
+        :param save_dir:
+        :param outputs_class:
+        :param inputs_vocab:
+        :param inputs_len:
+        :param cls:
         :return:
         """
 
-        inp = Input(shape=(maxlen, ))
+        inp = Input(shape=(inputs_len,))
 
         op = EmbeddingBiLSTM(name='BiLSTM',
-                             embedding_word_number=max_features,
+                             embedding_word_number=inputs_vocab,
                              embedding_vector_length=128,
                              embedding_dropout=None,
                              embedding_kwargs=None,
@@ -165,7 +179,7 @@ class RunnerBiLSTM(object):
 
                              dense=[
                                  {'units': 50, 'activation': 'relu', 'dropout': 0.1, 'name': 'd'},
-                                 {'units': 6, 'activation': 'sigmoid', 'dropout': 0.1, 'name': 'd'}
+                                 {'units': len(outputs_class), 'activation': 'sigmoid', 'dropout': 0.1, 'name': 'd'}
                              ]
                              )(inp)
 
@@ -175,43 +189,108 @@ class RunnerBiLSTM(object):
                       optimizer='adam',
                       metrics=['accuracy'])
 
-        return cls(model, save_path)
+        # 保存参数
+        params_dict = {
+            'save_dir': save_dir,
+            'inputs_len': inputs_len,
+            'outputs_class': outputs_class,
+        }
+
+        try:
+            os.makedirs(save_dir)
+        except OSError:
+            pass
+
+        with open(file=os.path.join(save_dir, cls.params_filename), mode="w+") as fp:
+            fp.write(json.dumps(params_dict))
+
+        return cls(model, save_dir, inputs_len, inputs_vocab, outputs_class)
 
     @classmethod
-    def load(cls):
-        """"""
+    def load(cls, save_dir):
+        """
+        :return:
+        """
+        model_filepath = os.path.join(save_dir, cls.model_filename)
+        if not os.path.isfile(model_filepath):
+            model_filepath = model_filepath + '.best'
+        kwargs = json.load(open(file=model_filepath, mode="r"))
+        model = load_model(os.path.join(save_dir, cls.params_filename))
+        kwargs['model'] = model
+        return cls(**kwargs)
 
-    def fit(self, x, y, batch_size=32, epochs=10, verbose=1, callbacks=None,
-            validation_split=0., validation_data=None, shuffle=True,
+    def fit(self, batch_size=32, epochs=10, verbose=1, callbacks=None,
+            validation_split=0.1, validation_data=None, shuffle=True,
             class_weight=None, sample_weight=None, initial_epoch=0, **kwargs):
 
-        file_path = filepath("weights_base.best.hdf5")
+        if callbacks is None:
+            suffix = '.%d.checkpoint' '.epoch-{epoch:02d}' '.val_loss-{val_loss:.6f}' '.val_acc-{val_acc:.6f}'
 
-        checkpoint = ModelCheckpoint(file_path, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+            model_checkpoint_best_path = os.path.join(self._save_dir, self.model_filename + '.best')
 
-        early = EarlyStopping(monitor="val_loss", mode="min", patience=20)
+            model_checkpoint_better_path = os.path.join(self._save_dir, self.model_filename + suffix)
 
-        callbacks_list = [checkpoint, early] #early
+            checkpoint = ModelCheckpoint(
+                model_checkpoint_better_path, save_best_only=False, verbose=1)
 
-        model.fit(X_t, y, batch_size=batch_size, epochs=epochs, validation_split=0.1, callbacks=callbacks_list)
+            checkpoint_best = ModelCheckpoint(
+                model_checkpoint_best_path, save_best_only=True, monitor='val_loss', mode='min', verbose=1)
 
-    def predict(self,):
+            early = EarlyStopping(monitor="val_loss", mode="min", patience=20)
+
+            callbacks = [checkpoint, checkpoint_best, early]
+
+        self._model.fit(self._x, self._y, batch_size=batch_size, epochs=epochs, verbose=verbose, callbacks=callbacks,
+                        validation_split=validation_split, validation_data=validation_data, shuffle=shuffle,
+                        class_weight=class_weight, sample_weight=sample_weight, initial_epoch=initial_epoch, **kwargs)
+
+        return self
+
+    def predict(self,
+                batch_size=None,
+                verbose=0,
+                steps=None):
         """
 
         :return:
         """
-        y_test = model.predict(X_te)
+        self._y = self._model.predict(self._x, batch_size=batch_size, verbose=verbose, steps=steps)
+        return self
 
-        sample_submission = pd.read_csv(filepath("sample_submission.csv"))
+    def submit(self, save_path):
 
-        sample_submission[list_classes] = y_test
+        sample_submission = pd.DataFrame(columns=['id'] + self._outputs_class)
+        sample_submission['id'] = self._idx
+        sample_submission[self._outputs_class] = self._y
+        sample_submission.to_csv(save_path, index=False)
 
-        sample_submission.to_csv(filepath("baseline.csv"), index=False)
+    def set_idx(self, idx):
+        self._idx = idx
+        return self
+
+    def set_x(self, x):
+        self._x = x
+        return self
+
+    def set_y(self, y):
+        self._y = y
+        return self
+
+    @property
+    def idx(self):
+        return self._idx
+
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
 
 
 def get_model():
-
-    inp = Input(shape=(maxlen, ))
+    inp = Input(shape=(maxlen,))
 
     op = EmbeddingBiLSTM(name='BiLSTM',
                          embedding_word_number=max_features,
@@ -241,30 +320,35 @@ model = get_model()
 batch_size = 32
 epochs = 2
 
-
-file_path = filepath("weights_base.best.hdf5")
-
-checkpoint = ModelCheckpoint(file_path, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
-
-early = EarlyStopping(monitor="val_loss", mode="min", patience=20)
-
-
-callbacks_list = [checkpoint, early] #early
-model.fit(X_t, y, batch_size=batch_size, epochs=epochs, validation_split=0.1, callbacks=callbacks_list)
-
-model.load_weights(file_path)
-
-y_test = model.predict(X_te)
-
-
-sample_submission = pd.read_csv(filepath("sample_submission.csv"))
-
-sample_submission[list_classes] = y_test
-
-
-sample_submission.to_csv(filepath("baseline.csv"), index=False)
+# file_path = filepath("weights_base.best.hdf5")
+#
+# checkpoint = ModelCheckpoint(file_path, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+#
+# early = EarlyStopping(monitor="val_loss", mode="min", patience=20)
+#
+#
+# callbacks_list = [checkpoint, early] #early
+# model.fit(X_t, y, batch_size=batch_size, epochs=epochs, validation_split=0.1, callbacks=callbacks_list)
+#
+# model.load_weights(file_path)
+#
+# y_test = model.predict(X_te)
+#
+#
+# sample_submission = pd.read_csv(filepath("sample_submission.csv"))
+#
+# sample_submission[list_classes] = y_test
+#
+#
+# sample_submission.to_csv(filepath("baseline.csv"), idx=False)
 
 
 if __name__ == '__main__':
     """
     """
+    Runner\
+        .init(save_dir=filepath("BiLSTM"),
+              inputs_len=maxlen, inputs_vocab=max_features,
+              outputs_class=["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"])\
+        .set_x(X_t).set_y(y).fit(batch_size=batch_size, epochs=epochs, validation_split=0.1)\
+        .set_x(X_te).set_idx(test['id']).predict(verbose=True).submit("123.csv")
